e18040a4cce0a4996f5246ce3e48346dc9f509ee
Matteo <mattipiffi@gmail.com>
2024-02-06 16:01:06 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,28 @@
 const express = require('express');
+var mysql = require('mysql');
 const app = express();
 const port = 16000;
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
+});
+
+app.get('/db', (req, res) => {
+    var con = mysql.createConnection({
+        host: "10.30.0.10",
+        user: "root",
+        password: process.env.DBpass,
+        database: "piffa"
+    });
+
+    con.connect(function (err) {
+        if (err) throw err;
+        con.query("SELECT * FROM test", function (err, result, fields) {
+            if (err) throw err;
+            res.send(result);
+        });
+    }
+    );
 });
 
 app.listen(port, () => {
